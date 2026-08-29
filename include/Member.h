1312bfe4292079be3b67a039765fbfcc2214d9c7
Ahmed <a7md1@3bdo.com>
2026-08-29 17:00:50 +0300

    Add Person and Member modules

---
+++ include/Member.h
@@ -0,0 +1,32 @@
+﻿#ifndef MEMBER_H
+#define MEMBER_H
+
+#include "Person.h"
+#include "Book.h"
+#include "funcs.h"
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Member : public Person {
+public:
+    vector<string> books;
+    int limit;
+
+    Member();
+    Member(int id, const string &name, const string &password, int limit = 3);
+
+    bool canBorrow() const;
+    bool borrowBook(Book *catalog, size_t catalogSize, string query);
+    bool returnBook(const string &titleOrISBN);
+    double calculateFine(int daysLate, double ratePerDay) const;
+    void displayInfo() const;
+
+    const vector<string> &getBooks() const;
+    int getLimit() const;
+    void setLimit(int newLimit);
+};
+
+#endif // MEMBER_H
---
+++ include/Person.h
@@ -0,0 +1,32 @@
+﻿#ifndef PERSON_H
+#define PERSON_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Person {
+protected:
+    int id;
+    string name;
+    string password;
+
+public:
+    Person();
+    Person(int id, const string &name, const string &password);
+    virtual ~Person() = default;
+
+    int getId() const;
+    string getName() const;
+    string getPassword() const;
+
+    void setId(int newId);
+    void setName(const string &newName);
+    void setPassword(const string &newPassword);
+
+    bool checkPassword(const string &pass) const;
+    void displayInfo() const;
+};
+
+#endif // PERSON_H
